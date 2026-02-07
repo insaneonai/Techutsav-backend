15b14b75e0d6fa1d5feafdd750794303662a77d5
Jeydev <insaneonai@gmail.com>
2026-02-07 14:02:15 +0530

    initial commit

--- database/db.js
+++ database/db.js
@@ -1,13 +1,12 @@
-'use strict';
+"use strict";
 
-import mongoose from 'mongoose';
-import { DB_URL } from '../constants.js';
+import mongoose from "mongoose";
 
-mongoose.connect(DB_URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
+mongoose.connect(`${process.env.DB_URL}${process.env.DB_NAME}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-console.info('connected to db');
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+console.info("connected to db");
--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-'use strict';
+"use strict";
 
-import express from 'express';
-import * as database from './database/db.js';
-import * as cron from './cron/cron.js';
-import {HOST} from './constants.js';
-import router from './routes/routes.js';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import "dotenv/config";
+import express from "express";
+import * as database from "./database/db.js";
+import router from "./routes/routes.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({ origin: [`http://${HOST}:3030`] , methods: ['POST', 'GET', 'PATCH', 'DELETE'], credentials:true}));
-app.use('/api',router);
+app.use(
+  cors({
+    origin: [`http://${process.env.HOST}:3030`],
+    methods: ["POST", "GET", "PATCH", "DELETE"],
+    credentials: true,
+  }),
+);
+app.use("/api", router);
 
-app.get('/', (req, res) => {
-    res.status(404).send("404 forbidden :(");
-})
+app.get("/", (req, res) => {
+  res.status(404).send("Vanakam da mapla!!!");
+});
 
-
-app.listen(8080,HOST, () => {
-    console.log(`Example app listening on port 8080`)
-})
+app.listen(8080, process.env.HOST, () => {
+  console.log(`Example app listening on port 8080`);
+});
